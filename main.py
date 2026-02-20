6539295e2a36525e957eae91b8110e5f08ac1368
rhythm029 <rhythmsinghal.2906@gmail.com>
2026-02-20 10:54:03 +0530

    Initial main.py with placeholder code

---
+++ main.py
@@ -0,0 +1,11 @@
+# Main Calculator File 
+# main.py
+
+# Placeholder functions (will import later)
+
+def main():
+    print("Calculator Application")
+    print("Features will be integrated after merging.")
+
+if __name__ == "__main__":
+    main()
